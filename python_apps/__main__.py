53afb8b547f5393ca57aa282d5ed2e5e5884fb50
JakubKorytko <jakub.korytko@o2.pl>
2023-11-21 09:10:47 +0100

    Move apps directory to the root folder

--- python_apps/__main__.py
+++ python_apps/__main__.py
@@ -10,9 +10,15 @@ def main():
     """Reads the apps folder and runs the menu."""
 
     working_dir = os_path.dirname(os_path.realpath(__file__))
+
     apps_folder = "apps"
+    parent_folder = os_path.join(working_dir, "..")
 
     path = os_path.join(working_dir, apps_folder)
+    parent_path = os_path.join(parent_folder, apps_folder)
+
+    if os_path.isdir(parent_path):
+        path = parent_path
 
     folders = Explorer.get_data(path)
 
